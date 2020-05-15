f5476e92fb83926b1f7e56626d494dabfb97209d
Illya Datsyuk <ilya.datsyuk@gmail.com>
2020-05-15 21:16:19 +0300

    Add controller

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CinemaTA.Models;
+
+namespace CinemaTA.Controllers
+{
+    [Route("api/Movies")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public void Get(int id)
+        {
+        }
+
+        [HttpPost]
+        public void PostMovie(Movie movie)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, Movie movie)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
