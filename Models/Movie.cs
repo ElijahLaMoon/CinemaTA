0952a3ff9c194312c9c39061a6b0d738db917e9b
Illya Datsyuk <ilya.datsyuk@gmail.com>
2020-05-15 18:35:42 +0300

    Add Movie model

---
+++ Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaTA.Models
+{
+    public class Movie
+    {
+        [Key]
+        public uint Id { get; private set; }
+        [Required]
+        public string Title { get; private set; }
+        [Required]
+        public string Director { get; private set; }
+        [Required]
+        public uint ReleaseYear { get; private set; }
+
+        // This field is used for providing correct IDs without user interaction.
+        // Basically, replaces autoincrement ID functionality available in most databases
+        private static uint movieCounter;
+
+        public Movie() { }
+        static Movie() => movieCounter = 0;
+        public Movie(string title, string director, uint releaseYear)
+        {
+            movieCounter++;
+            this.Id = movieCounter;
+            this.Title = title;
+            this.Director = director;
+            this.ReleaseYear = releaseYear;
+        }
+    }
+}
