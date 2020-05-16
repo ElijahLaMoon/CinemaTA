fc5c800d4e3bc322bbce577ccbb7c107fa3b0a3d
Illya Datsyuk <ilya.datsyuk@gmail.com>
2020-05-16 17:43:36 +0300

    Update model

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -12,20 +12,5 @@ namespace CinemaTA.Models
         public string Director { get; set; }
         [Required]
         public uint ReleaseYear { get; set; }
-
-        // This field is used for providing correct IDs without user interaction.
-        // Basically, replaces autoincrement ID functionality available in most databases
-        private static int movieCounter;
-
-        public Movie() { }
-        static Movie() => movieCounter = 0;
-        public Movie(string title, string director, uint releaseYear)
-        {
-            movieCounter++;
-            this.Id = movieCounter;
-            this.Title = title;
-            this.Director = director;
-            this.ReleaseYear = releaseYear;
-        }
     }
 }
