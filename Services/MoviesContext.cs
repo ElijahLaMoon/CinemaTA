0a5ac449c4acecfc62f701f7fee3019f765bbd68
Illya Datsyuk <ilya.datsyuk@gmail.com>
2020-05-16 17:08:55 +0300

    Add SQL Server database

---
+++ Services/MoviesContext.cs
@@ -0,0 +1,17 @@
+﻿using CinemaTA.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace CinemaTA.Services
+{
+    public class MoviesContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public MoviesContext(DbContextOptions<MoviesContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
